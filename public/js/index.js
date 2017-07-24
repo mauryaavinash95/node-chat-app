6f7e817f9648f3a8e8e76e95c23313cab36df72d
Avinash Maurya <mauryaavinash95@gmail.com>
2017-07-24 17:01:58 +0530

    Greet new user and alert others done

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,12 @@
 var socket = io();
+
 socket.on('connect', function () {
     console.log("Connected to the server");
-
-    // Send a message from the client to the server
-    // socket.emit('createMessage', {
-    //     from: 'to@server.com',
-    //     text: "Hello from client",
-    // });
-
 });
 
-socket.on('newMessage', function (messageContent) {
-    console.log('New Message recieved at client', messageContent);
-})
-
+socket.on('newMessage', function (message) {
+    console.log("newMessage recieved is : ", message);
+});
 
 socket.on('disconnect', function () {
     console.log("Disconnected from the server");
--- server/server.js
+++ server/server.js
@@ -21,15 +21,35 @@ io.on('connection', (socket) => {
     //     text: 'This is newMessage Event from server',
     //     createdAt: (new Date()).toString(),
     // });
+    // socket.emit from admin "Welcome to the chat app"
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the CHAT app",
+        createdAt: (new Date().getTime()),
+    });
 
+    // socket.broadcast.emit from Admin "New user joined".
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user has joined the chat app",
+        createdAt: (new Date().getTime()),
+    })
 
     socket.on('createMessage', function (messageContent) {
         console.log('New Message recieved at Server', messageContent);
+        //  io.emit will broadcast the message to each and every socket including the one who sent it.
         io.emit('newMessage', {
             from: messageContent.from,
             text: messageContent.text,
-            createdAt : (new Date().getTime()),
+            createdAt: (new Date().getTime()),
         });
+
+        //socket.broadcast is used to emit the message to everyone but the origininating socket/client.
+        // socket.broadcast.emit('newMessage', {
+        //     from: messageContent.from,
+        //     text: messageContent.text,
+        //     createdAt: (new Date().getTime()),
+        // })
     })
 
     socket.on('disconnect', () => {
