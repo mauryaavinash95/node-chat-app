b3f526d19b0d0790c70c035bf5c70495d379983d
Avinash Maurya <mauryaavinash95@gmail.com>
2017-07-25 11:13:30 +0530

    Checking geolocation online

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,15 @@ socket.on('newMessage', function (message) {
     console.log("newMessage recieved is : ", message);
     var li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
 
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_">My current location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
     jQuery('#messages').append(li);
 });
 
@@ -16,13 +24,33 @@ socket.on('disconnect', function () {
     console.log("Disconnected from the server");
 });
 
-jQuery('#message-form').on('submit', function(e){
+jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
-    
+
     socket.emit('createMessage', {
         from: 'user',
         text: jQuery('[name=message]').val()
-    }, function(){
+    }, function () {
         console.log('Ack recieved');
     })
+});
+
+
+var locationButton = jQuery('#sendLocation');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            console.log(position);
+            alert(position);
+            // socket.emit('createLocationMessage', {
+            //     latitude: position.coords.latitude,
+            //     longitude: position.coords.longitude,
+            // })
+        }, function (err) {
+            alert('Unable to fetch your coordinates', err);
+        });
 });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 const publicPath = path.join(__dirname, '../public');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 // console.log(publicPath);
 var app = express();
 var server = http.createServer(app);
@@ -34,7 +34,11 @@ io.on('connection', (socket) => {
         //     text: messageContent.text,
         //     createdAt: (new Date().getTime()),
         // })
-    })
+    });
+
+    socket.on('createLocationMessage', (coords)=>{
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
 
     socket.on('disconnect', () => {
         console.log("Client disconnected");
